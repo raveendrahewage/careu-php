72b75ebd3d54aef972fde58c6313c700f994415a
LManjitha <lmanjithah@gmail.com>
2020-11-03 16:51:26 +0530

    add php

---
+++ loginApp.php
@@ -0,0 +1,14 @@
+<?php
+require "conn.php";
+
+$userName = $_POST['userName'];
+$password = $_POST['password'];
+$mysql_qry = "select * from servicerequester where username like '$userName' and password like '$password' ";
+$result = mysqli_query($conn,$mysql_qry);
+if(mysqli_num_rows($result)>0){
+    echo "login success";
+}
+else{
+    echo "login failed";
+}
+?>
